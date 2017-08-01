231e9a91e729f2eeaac29a772c81b1b32d85533c
Jose Velez <jose.velez363@gmail.com>
2017-08-01 18:13:05 -0400

    CSS files working now.

--- server.js
+++ server.js
@@ -4,15 +4,13 @@
 var express = require("express");
 var path = require("path");
 
-var path    = require("path");
-
 
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 
 //==================================
